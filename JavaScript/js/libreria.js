204331829ba34ca1acce870164c23843a5bf4b3f
PatriciaSF <pt70587300@idat.edu.pe>
2022-03-09 14:03:15 -0500

    Subiendo las actividades

---
+++ JavaScript/js/libreria.js
@@ -0,0 +1,9 @@
+//alert("Se ve el mensaje?")
+//document.write("Se ve el mensaje?")
+//console.log("Ultima prueba!");
+var nombre = 'Patricia',
+numero1 = 5, numero2 = 2
+resultado=numero1 + numero2;
+
+document.write(nombre);
+document.write(resultado);
